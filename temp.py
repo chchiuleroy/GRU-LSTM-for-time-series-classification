b791650c46913aca0c539137dd6b7f9de24d50d9
Kun-Min Chiu <chchiuleroy@gmail.com>
2019-10-30 12:59:07 +0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -48,7 +48,7 @@ id1 = setdiff1d(array(list(range(x.shape[0]))), id0)
 train_y, test_y, train_x, test_x = y[id1], y[id0], x[id1, :, :], x[id0, :, :]
 threshold = unique(train_y, return_counts = True)[1]/train_y.shape[0]
 
-def grumodel(filter0, filter1, rate0, rate1):
+def lstmmodel(filter0, filter1, rate0, rate1):
     model = Sequential()
     model.add(LSTM(units = filter0, input_shape = (n_steps_in, n_features), return_sequences = True))
     model.add(BatchNormalization())
@@ -83,7 +83,7 @@ def objective0(filter0, filter1, rate0, rate1, b_size):
     for train_index, valid_index in skf.split(train_x, train_y):
         x_train, x_valid = train_x[train_index], train_x[valid_index]
         y_train, y_valid = train_y[train_index], train_y[valid_index]
-        model = grumodel(filter0, filter1, rate0, rate1)
+        model = lstmmodel(filter0, filter1, rate0, rate1)
         fitting = model_fit(model, x_train, y_train, x_valid, y_valid, b_size) 
     return -fitting
 
@@ -91,7 +91,7 @@ res0 = gp_minimize(objective0, space, n_calls = 10, acq_func = 'EI', n_points = 
 obj = res0.fun
 best = res0.x
 
-model = grumodel(*best[0:4])
+model = lstmmodel(*best[0:4])
 adam = Adam(lr = 0.005)  
 model.compile(loss = 'categorical_crossentropy', optimizer = adam, metrics = ['accuracy'])
 callback = EarlyStopping(monitor = 'acc', patience = 10, verbose = 1, mode = 'auto')
